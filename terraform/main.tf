13336e903e3c84312813a5133f0ad6ee5f9104e7
Reshmaoviya <reshmaoviya07@gmail.com>
2025-07-23 22:00:22 +0530

    Manual Trigger Terraform deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,16 @@
-provider "aws" {
-  region     = "us-east-2"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
-
 resource "aws_instance" "strapi" {
-  ami           = "ami-08c40ec9ead489470" # Ubuntu 22.04 LTS (Ohio)
-  instance_type = "t2.micro"
-  key_name      = var.key_name
+  ami                    = "ami-0c55b159cbfafe1f0"  # ✅ Replace this with the correct one
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
-              apt update -y
-              apt install docker.io -y
-              systemctl start docker
-              docker login -u ${var.dockerhub_username} -p ${var.docker_password}
-              docker pull ${var.dockerhub_username}/strapi-ec2:latest
-              docker run -d -p 80:1337 ${var.dockerhub_username}/strapi-ec2:latest
-            EOF
+              sudo apt update -y
+              sudo apt install docker.io -y
+              sudo systemctl start docker
+              sudo docker run -d -p 80:1337 ${var.dockerhub_username}/strapi-ec2
+              EOF
 
   tags = {
     Name = "StrapiEC2"
