aa436a1d4619ec0c8dd05ebbfaa2788a864ad6a2
Reshmaoviya <reshmaoviya07@gmail.com>
2025-07-23 21:36:47 +0530

    Manual Trigger Terraform deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,35 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
+variable "key_name" {}
+variable "dockerhub_username" {}
+variable "docker_password" {}
+
 resource "aws_instance" "strapi" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 (Ohio)
-  instance_type = "t2.micro"
-  key_name      = var.key_name
+  ami                    = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (Ohio) - adjust if needed
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              usermod -a -G docker ec2-user
+              sudo yum update -y
+              sudo amazon-linux-extras install docker -y
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+              docker login -u ${var.dockerhub_username} -p ${var.docker_password}
+              docker pull reshh07/strapi-ec2:latest
               docker run -d -p 80:1337 reshh07/strapi-ec2:latest
               EOF
 
   tags = {
-    Name = "Strapi-Instance"
+    Name = "StrapiEC2"
   }
+}
+
+output "strapi_public_ip" {
+  value = aws_instance.strapi.public_ip
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,24 @@
-variable "aws_access_key" {
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key ID"
   type        = string
-  description = "AWS Access Key"
 }
 
-variable "aws_secret_key" {
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
   type        = string
-  description = "AWS Secret Key"
 }
 
 variable "key_name" {
+  description = "Name of the existing AWS key pair to use for EC2"
   type        = string
-  description = "EC2 Key Pair Name"
+}
+
+variable "dockerhub_username" {
+  description = "Docker Hub username"
+  type        = string
+}
+
+variable "docker_password" {
+  description = "Docker Hub password or access token"
+  type        = string
 }
